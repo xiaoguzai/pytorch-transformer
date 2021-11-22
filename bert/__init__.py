f56baa847a36c565e23eca40ee893037b2da26d2
xiaoguzai <31726901+xiaoguzai@users.noreply.github.com>
2021-11-22 17:18:20 +0800

    Add files via upload

---
+++ bert/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2021/11/22
+# @Author  : xiaoguzai
+# @Email   : 474551240@qq.com
+# @File    : __init__.py.py
+# @github  : https://github.com/xiaoguzai
+
+from .loader_bert_base import load_bert_base_data
+from .loader_bert_wwm import load_bert_wwm_data
+from .bert import BertConfig,Bert
