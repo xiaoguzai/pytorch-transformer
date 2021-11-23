b81d18617eedc122f4501e9ffe75ea109481554f
xiaoguzai <31726901+xiaoguzai@users.noreply.github.com>
2021-11-23 10:36:14 +0800

    Delete roberta directory

--- roberta/__init__.py
+++
@@ -1,7 +0,0 @@
-## Author:gutianyu
-## Email:474551240@qq.com
-## Data:2021/11/22
-
-from .roberta import RobertaConfig,Roberta
-from .loader_roberta_base import load_roberta_base_data
-from .loader_roberta_wwm import load_roberta_wwm_data
