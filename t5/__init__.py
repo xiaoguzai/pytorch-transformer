40ab67f53d1f0c2340a47e49a59781a939077bff
xiaoguzai <31726901+xiaoguzai@users.noreply.github.com>
2022-06-01 12:29:20 +0800

    Add files via upload

---
+++ t5/__init__.py
@@ -0,0 +1,7 @@
+## Author:gutianyu
+## Email:474551240@qq.com
+## Data:2021/11/22
+
+from .t5 import T5Config,T5Generation
+from .t5 import new_greedy_generate,greedy_generate
+from .loader_t5 import load_t5_generation_data,load_t5_model_data
